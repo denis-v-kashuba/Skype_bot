55599171e8240e285c6c6a206687bfc6b75ed23a
Denis Kashuba <deni.kashuba@gmail.com>
2016-08-20 15:19:57 +0300

    Init

---
+++ app.js
@@ -0,0 +1,36 @@
+/**
+ * Created by deniskashuba on 20.08.16.
+ */
+
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+	console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+	appId: process.env.MICROSOFT_APP_ID,
+	appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+	session.send("Hello World");
+});
+
+bot.dialog('/test', function (session) {
+	session.send("Test accepted");
+});

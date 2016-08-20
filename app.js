6414b2b9b635dec86c82d9092fd53cf124d3d2ef
Denis Kashuba <deni.kashuba@gmail.com>
2016-08-20 18:54:17 +0300

    Dev commit

--- app.js
+++ app.js
@@ -2,16 +2,51 @@
  * Created by deniskashuba on 20.08.16.
  */
 
+//var restify = require('restify');
+//var builder = require('botbuilder');
+//
+////=========================================================
+//// Bot Setup
+////=========================================================
+//
+//// Setup Restify Server
+//var server = restify.createServer();
+//server.listen(process.env.port || process.env.PORT || 3978, function () {
+//	console.log('%s listening to %s', server.name, server.url);
+//});
+//
+//// Create chat bot
+//var connector = new builder.ChatConnector({
+//	appId: process.env.MICROSOFT_APP_ID,
+//	appPassword: process.env.MICROSOFT_APP_PASSWORD
+//});
+//var bot = new builder.UniversalBot(connector);
+//server.post('/api/messages', connector.listen());
+//
+////=========================================================
+//// Bots Dialogs
+////=========================================================
+//
+//bot.dialog('/', function (session) {
+//	session.send("Hello World");
+//});
+
 var restify = require('restify');
 var builder = require('botbuilder');
+var express = require('express');
+var app = express();
 
 //=========================================================
 // Bot Setup
 //=========================================================
 
+//app.get('/', function(req, res){
+//	res.send('Hello');
+//});
+
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 8080, function () {
+server.listen(process.env.port || process.env.PORT || 3978, function () {
 	console.log('%s listening to %s', server.name, server.url);
 });
 
@@ -20,9 +55,14 @@ var connector = new builder.ChatConnector({
 	appId: "d270c2be-1f08-48d6-b883-48440f6ecec2",
 	appPassword: "qCzUFFSNZ3rN8LDQ8hhg6Wp"
 });
+//var connector = new builder.ChatConnector({
+//	appId: "",
+//	appPassword: ""
+//});
 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
+server.get('/api/messages', connector.listen());
 
 //Bot on
 bot.on('contactRelationUpdate', function (message) {
@@ -53,6 +93,7 @@ String.prototype.contains = function(content){
 }
 
 bot.dialog('/', function (session) {
+
 	if(session.message.text.toLowerCase().contains('hello')){
 		session.send('Hey, How are you?');
 	}else if(session.message.text.toLowerCase().contains('help')){
@@ -62,4 +103,5 @@ bot.dialog('/', function (session) {
 	}else{
 		session.send('Sorry I don\'t understand you...');
 	}
+
 });

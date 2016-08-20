da5c6c40e2ca358871c269967198f6efbac2c123
Denis Kashuba <deni.kashuba@gmail.com>
2016-08-20 18:11:53 +0300

    Small changes

--- app.js
+++ app.js
@@ -11,26 +11,55 @@ var builder = require('botbuilder');
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
+server.listen(process.env.port || process.env.PORT || 8080, function () {
 	console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat bot
 var connector = new builder.ChatConnector({
-	appId: process.env.MICROSOFT_APP_ID,
-	appPassword: process.env.MICROSOFT_APP_PASSWORD
+	appId: "Your App ID Here",
+	appPassword: "Your App Password Here"
 });
+
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
+
+//Bot on
+bot.on('contactRelationUpdate', function (message) {
+	if (message.action === 'add') {
+		var name = message.user ? message.user.name : null;
+		var reply = new builder.Message()
+			.address(message.address)
+			.text("Hello %s... Thanks for adding me. Say 'hello' to see some great demos.", name || 'there');
+		bot.send(reply);
+	} else {
+		// delete their data
+	}
+});
+
+bot.on('typing', function (message) {
+	// User is typing
+});
+
+bot.on('deleteUserData', function (message) {
+	// User asked to delete their data
+});
 
 //=========================================================
 // Bots Dialogs
 //=========================================================
+String.prototype.contains = function(content){
+	return this.indexOf(content) !== -1;
+}
 
 bot.dialog('/', function (session) {
-	session.send("Hello World");
-});
-
-bot.dialog('/test', function (session) {
-	session.send("Test accepted");
+	if(session.message.text.toLowerCase().contains('hello')){
+		session.send('Hey, How are you?');
+	}else if(session.message.text.toLowerCase().contains('help')){
+		session.send('How can I help you?');
+	}else if(session.message.text.toLowerCase().contains('bogdan')){
+		session.send('Работай лучше!');
+	}else{
+		session.send('Sorry I don\'t understand you...');
+	}
 });
